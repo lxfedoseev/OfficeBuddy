5c3941d8c764cacf58b04abc7369afab21ee7f7f
Alex Fedoseev <lxfedoseev@gmail.com>
2018-11-14 22:05:44 +0300

    chapter 24

--- OfficeBuddy/ContainerViewController.swift
+++ OfficeBuddy/ContainerViewController.swift
@@ -61,10 +61,12 @@ class ContainerViewController: UIViewController {
     view.addSubview(menuViewController.view)
     menuViewController.didMove(toParentViewController: self)
     
+    menuViewController.view.layer.anchorPoint.x = 1.0
     menuViewController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: view.frame.height)
     
     let panGesture = UIPanGestureRecognizer(target:self, action:#selector(ContainerViewController.handleGesture(_:)))
     view.addGestureRecognizer(panGesture)
+    setMenu(toPercent: 0.0)
   }
   
   func handleGesture(_ recognizer: UIPanGestureRecognizer) {
@@ -78,6 +80,9 @@ class ContainerViewController: UIViewController {
     case .began:
       let isOpen = floor(centerViewController.view.frame.origin.x/menuWidth)
       isOpening = isOpen == 1.0 ? false: true
+      // Improve the look of the opening menu
+      menuViewController.view.layer.shouldRasterize = true
+      menuViewController.view.layer.rasterizationScale = UIScreen.main.scale
       
     case .changed:
       self.setMenu(toPercent: isOpening ? progress: (1.0 - progress))
@@ -98,7 +103,7 @@ class ContainerViewController: UIViewController {
           self.setMenu(toPercent: targetProgress)
         },
         completion: {_ in
-          
+          self.menuViewController.view.layer.shouldRasterize = false
         }
       )
       
@@ -122,7 +127,40 @@ class ContainerViewController: UIViewController {
   
   func setMenu(toPercent percent: CGFloat) {
     centerViewController.view.frame.origin.x = menuWidth * CGFloat(percent)
-    menuViewController.view.frame.origin.x = menuWidth * CGFloat(percent) - menuWidth
+    menuViewController.view.layer.transform = menuTransform(percent: percent)
+    menuViewController.view.alpha = CGFloat(max(0.2, percent))
+    
+    let centerVC = centerViewController.viewControllers.first as? CenterViewController
+    if let menuButton = centerVC?.menuButton {
+        menuButton.imageView.layer.transform = buttonTransform(percent: percent)
+    }
   }
   
+    func menuTransform(percent: CGFloat) -> CATransform3D {
+        var identity = CATransform3DIdentity
+        identity.m34 = -1.0/1000
+        let remainingPercent = 1.0 - percent
+        let angle = remainingPercent * .pi * -0.5
+        
+        let rotationTransform = CATransform3DRotate(
+            identity, angle, 0.0, 1.0, 0.0)
+        let translationTransform = CATransform3DMakeTranslation(
+            menuWidth * percent, 0, 0)
+        
+        return CATransform3DConcat(
+            rotationTransform, translationTransform)
+        
+    }
+    
+    func buttonTransform(percent: CGFloat) -> CATransform3D {
+        
+        var identity = CATransform3DIdentity
+        identity.m34 = -1.0/1000
+        
+        let angle = percent * .pi
+        let rotationTransform = CATransform3DRotate(identity, angle, 1.0, 1.0, 0.0)
+        
+        return rotationTransform
+    }
+    
 }
